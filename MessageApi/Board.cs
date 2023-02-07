8d1971eec6d385731500a403109f1d4e521a247e
Athea DeLing <almdeling@gmail.com>
2023-02-06 22:13:47 -0500

    add boiler plate
    Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>
    Co-authored-by: Bai  Jaitrong <bd2jaitrong@gmail.com>

---
+++ MessageApi/Board.cs
@@ -0,0 +1,12 @@
+namespace MessageApi;
+
+public class Board
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
